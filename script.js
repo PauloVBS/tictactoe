172894d47a2fc709086ec02a464a7bf2c8447c8e
slasher314 <paulo.vb.souza@unesp.br>
2022-01-31 23:37:41 -0300

    almost all done

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ atualizaMostrador();
 inicializarEspacos();
 
 function atualizaMostrador(){
-    if(gameOver){return;}
+    if( gameOver == true){return};
     if(playTime == player1){
-        let player = document.querySelectorAll("div#mostrador img")[0];
-        player.setAttribute("src", "imagens/x.png");
+        var player = document.querySelector("img#xouo");
+        player.setAttribute("src", "./imagens/x.png");
     }else{
-        let player = document.querySelectorAll("div#mostrador img")[0];
-    player.setAttribute("src", "imagens/o.png");
+        var player = document.querySelector("img#xouo");
+    player.setAttribute("src", "./imagens/o.png");
 
 
     }
@@ -26,19 +26,59 @@ function inicializarEspacos(){
         
         espaco[i].addEventListener("click",function(){
             if (gameOver){return;}
-            if(this.getElementsByTagName("img").lenght == 0){
+            if(this.getElementsByTagName("img").length == 0){
                 if(playTime ==  player1){
-                    this.innerHTML="<img src='imagens/x.png'>";
+                    this.innerHTML="<img src='./imagens/x.png'>";
                     this.setAttribute("jogada", player1);
                     playTime = player2;
                 }else{
-                    this.innerHTML="<img src='imagens/o.png'>";
+                    this.innerHTML="<img src='./imagens/o.png'>";
                     this.setAttribute("jogada", player2);
                     playTime = player1;
 
                 }
                 
             }atualizaMostrador();
+            verificarVencedor();
         });
     }
 }
+ async function verificarVencedor(){
+
+
+    var a1 = document.getElementById("a1").getAttribute("jogada");
+    var a2 = document.getElementById("a2").getAttribute("jogada");
+    var a3 = document.getElementById("a3").getAttribute("jogada");
+
+    var b1 = document.getElementById("b1").getAttribute("jogada");
+    var b2 = document.getElementById("b2").getAttribute("jogada");
+    var b3 = document.getElementById("b3").getAttribute("jogada");
+    
+    var c1 = document.getElementById("c1").getAttribute("jogada");
+    var c2 = document.getElementById("c2").getAttribute("jogada");
+    var c3 = document.getElementById("c3").getAttribute("jogada");
+
+    var vencedor ="";
+
+    if((a1==b1 && a1==c1 && a1!="")||(a1 ==a2 && a1 == a3 && a1 !="")||(a1 == b2 && a1 == c3 && a1 != "")){
+    vencedor = a1;
+}else if((b1 == b2 && b1 == b3 && b1!= "")||(a2 == b2 && a2 == c2 && a2 !="")||(a3 == b2 && a3 == c1 && a3 != "")){
+    vencedor = b2;
+}else if((a3 == b3 && a3 == c3 && a3!= "")||(c1 == c2 && c1 == c3 && c1 != ""));{
+    vencedor = c3;
+    } 
+    if (vencedor != ""){
+     gameOver = true;
+
+     await sleep(50);
+     alert(`O vencedor foi o : ${vencedor}`)
+    }else{
+
+    }
+};
+
+function sleep(ms){
+
+    return new Promise(resolve => setTimeout(resolve,ms));
+}
+
